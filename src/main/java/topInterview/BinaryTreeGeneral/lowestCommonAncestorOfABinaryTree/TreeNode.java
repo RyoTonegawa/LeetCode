59f8081a9e773398a7b8fc145bc8489048125727
RyoTonegawa <tonegawa622@gmail.com>
2024-12-11 21:23:44 +0900

    LowestCommonAncestor

---
+++ src/main/java/topInterview/BinaryTreeGeneral/lowestCommonAncestorOfABinaryTree/TreeNode.java
@@ -0,0 +1,11 @@
+package main.java.topInterview.binaryTreeGeneral.lowestCommonAncestorOfABinaryTree;
+
+public class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+}
