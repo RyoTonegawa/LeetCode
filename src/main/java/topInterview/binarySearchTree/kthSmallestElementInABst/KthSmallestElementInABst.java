722780ade1193f152a931252171df515e7f0de17
RyoTonegawa <tonegawa622@gmail.com>
2024-12-15 20:35:24 +0900

    kth Smallest Element in a BST

--- src/main/java/topInterview/binarySearchTree/kthSmallestElementInABst/KthSmallestElementInABst.java
+++ src/main/java/topInterview/binarySearchTree/kthSmallestElementInABst/KthSmallestElementInABst.java
@@ -1,5 +1,24 @@
 package main.java.topInterview.binarySearchTree.kthSmallestElementInABst;
 
 public class KthSmallestElementInABst {
+  private int count = 0;
+  private int result = 0;
 
+  public int kthSmallest(TreeNode root, int k) {
+    inorderIterator(root, k);
+    return result;
+  }
+
+  public void inorderIterator(TreeNode node, int k) {
+    if (node == null) {
+      return;
+    }
+    inorderIterator(node.left, k);
+    this.count++;
+    if (count == k) {
+      result = node.val;
+      return;
+    }
+    inorderIterator(node.right, k);
+  }
 }
